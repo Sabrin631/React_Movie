0eef9b6de5f3325dd8a9d3576b66c6907f21710e
Sabrin631 <sabiriin.aden@icloud.com>
2025-05-16 23:10:41 +0200

    Fixed last thngs

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,20 +1,32 @@
-import "../CSS/MovieCard.css"; 
-
+import "../CSS/MovieCard.css";
+import { useMovieContext } from "../Contexts/MovieContext";
 
 function MovieCard({ movie }) {
-    function onLike() {
-        alert("clicked")
+    const { isFavorite, addToFavorites, removeFromFavorites } = useMovieContext();
+    const favorite = isFavorite(movie.id);
+
+    function onLike(e) {
+        e.preventDefault();
+        if (favorite) {
+            removeFromFavorites(movie.id);
+            
+        } else {
+            addToFavorites(movie);
+        }
     }
 
     return (
         <div className="movie-card">
             <div className="movie-poster">
                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-
                 <div className="movie-overlay">
-                    <button className="favorite-btn" onClick={onLike}>
-                        🤍
-                    </button>
+                <button
+  className={`favorite-btn ${favorite ? "active" : ""}`}
+  onClick={onLike}
+>
+  ♥
+</button>
+
                 </div>
             </div>
             <div className="movie-info">
@@ -22,7 +34,7 @@ function MovieCard({ movie }) {
                 <p>{movie.release_date?.split("-")[0]}</p>
             </div>
         </div>
-    )
+    );
 }
 
-export default MovieCard
+export default MovieCard;
--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,10 +1,27 @@
 import "../CSS/Favorites.css";
+import { useMovieContext } from "../Contexts/MovieContext"; 
+import MovieCard from "../components/MovieCard";  // Don't forget to import MovieCard!
 
 function Favorites() {
+  const { favorites } = useMovieContext(); 
+
+  if (favorites && favorites.length > 0) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="favorites-empty">
-      <h2>No favorite movies Yet</h2>
-      <p>Start adding movies to your favorite and they will appear here</p>
+      <h2>No favorite movies yet</h2>
+      <p>Start adding movies to your favorites and they will appear here.</p>
     </div>
   );
 }
